f89f4c0b5a361c62b19d8c205fa81cb86116e246
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-09-21 23:25:30 +1000

    Add rudimentary skeleton for the microservice.

---
+++ src/main/java/com/transroutownish/proto/bus/UrbanBusRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.transroutownish.proto.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanBusRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanBusRoutingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transroutownish/proto/bus/UrbanBusRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transroutownish.proto.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanBusRoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
