1583d14119a5521249cfb93e4d8472977900b1a0
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-09-22 18:10:00 +1000

    (1) Add description-version-copyright-license banner. (2) Reorder imports. (3) Add Javadoc comment blocks. (4) Replace tabs with spaces. (5) Add Vim modeline mantra.

--- src/main/java/com/transroutownish/proto/bus/UrbanBusRoutingApplication.java
+++ src/main/java/com/transroutownish/proto/bus/UrbanBusRoutingApplication.java
@@ -1,13 +1,38 @@
+/*
+ * src/main/java/com/transroutownish/proto/bus/UrbanBusRoutingApplication.java
+ * ============================================================================
+ * Urban bus routing microservice prototype. Version 0.0.1
+ * ============================================================================
+ * A Spring Boot-based application, designed and intended to be run
+ * as a microservice, implementing a simple urban bus routing prototype.
+ * ============================================================================
+ * Written by Radislav (Radicchio) Golubtsov, 2021
+ *
+ * (See the LICENSE file at the top of the source tree.)
+ */
+
 package com.transroutownish.proto.bus;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
+/**
+ * The startup class of the microservice.
+ *
+ * @version 0.0.1
+ * @since   0.0.1
+ */
 @SpringBootApplication
 public class UrbanBusRoutingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UrbanBusRoutingApplication.class, args);
-	}
-
+    /**
+     * The microservice entry point.
+     *
+     * @param args The array of command-line arguments.
+     */
+    public static void main(final String[] args) {
+        // Starting up the app.
+        SpringApplication.run(UrbanBusRoutingApplication.class, args);
+    }
 }
+
+// vim:set nu et ts=4 sw=4:
--- src/test/java/com/transroutownish/proto/bus/UrbanBusRoutingApplicationTests.java
+++ src/test/java/com/transroutownish/proto/bus/UrbanBusRoutingApplicationTests.java
@@ -1,13 +1,33 @@
+/*
+ * src/test/java/com/transroutownish/proto/bus/
+ * UrbanBusRoutingApplicationTests.java
+ * ============================================================================
+ * Urban bus routing microservice prototype. Version 0.0.1
+ * ============================================================================
+ * A Spring Boot-based application, designed and intended to be run
+ * as a microservice, implementing a simple urban bus routing prototype.
+ * ============================================================================
+ * Written by Radislav (Radicchio) Golubtsov, 2021
+ *
+ * (See the LICENSE file at the top of the source tree.)
+ */
+
 package com.transroutownish.proto.bus;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
 
+/**
+ * The test class for the microservice.
+ *
+ * @version 0.0.1
+ * @since   0.0.1
+ */
 @SpringBootTest
 class UrbanBusRoutingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    /** Dummy for a while... Does nothing. */
+    @Test
+    void contextLoads() {}
 }
+
+// vim:set nu et ts=4 sw=4:
