4079020467c6ff5b7ec4cc4e8b9207fc59f9123d
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2022-03-30 23:40:00 +1100

    Bump version number to 0.15.1.

--- src/main/java/com/transroutownish/proto/bus/UrbanBusRoutingApp.java
+++ src/main/java/com/transroutownish/proto/bus/UrbanBusRoutingApp.java
@@ -1,7 +1,7 @@
 /*
  * src/main/java/com/transroutownish/proto/bus/UrbanBusRoutingApp.java
  * ============================================================================
- * Urban bus routing microservice prototype. Version 0.15.0
+ * Urban bus routing microservice prototype. Version 0.15.1
  * ============================================================================
  * A Spring Boot-based application, designed and intended to be run
  * as a microservice, implementing a simple urban bus routing prototype.
@@ -33,7 +33,7 @@ import static com.transroutownish.proto.bus.UrbanBusRoutingControllerHelper.*;
 /**
  * The startup class of the microservice.
  *
- * @version 0.15.0
+ * @version 0.15.1
  * @since   0.0.1
  */
 @SpringBootApplication
